a42e13f413973d285fc5c34234298ad8e46bfa44
kristenspirka12 <krispi98@gmail.com>
2019-02-12 14:05:50 +0200

    Add files via upload

--- LinkedList/src/linkedList/MainLinkedList.java
+++ LinkedList/src/linkedList/MainLinkedList.java
@@ -1,5 +1,6 @@
 package linkedList;
 
+import java.util.Arrays;
 import java.util.LinkedList;
 
 public class MainLinkedList {
@@ -8,8 +9,14 @@ public class MainLinkedList {
 		LinkedList<String> llistobj = new LinkedList<String>();
 		llistobj.add(0, "tere");
 		llistobj.add(0, "teinesõna");
-		System.out.println(llistobj); 
+		System.out.println(llistobj);
 		
-		
+		//tagurpidi läbikäimine
+		/*for (int i = array.length - 2; i >= 0; i--) {
+			System.out.println(array[i]);
+			//"nihutamine" paremale
+			array[i+1] = array[i];
+		}
+		System.out.println(Arrays.toString(array)); */
 	}
 }
